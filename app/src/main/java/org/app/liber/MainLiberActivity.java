2376772b8a71c3423400658ae0a5202be23f6b54
PrashantM89 <prashantm61289@gmail.com>
2019-05-12 02:43:21 +0530

    Fine tuned user location display functionality.

--- app/src/main/java/org/app/liber/MainLiberActivity.java
+++ app/src/main/java/org/app/liber/MainLiberActivity.java
@@ -2,18 +2,19 @@ package org.app.liber;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
+import android.os.Build;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -25,12 +26,11 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import org.app.liber.Service.LocationService;
 import org.app.liber.activity.NotificationActivity;
 import org.app.liber.adapter.ViewPagerAdapter;
 import org.app.liber.helper.DatabaseHelper;
 import org.app.liber.helper.LocationHelper;
-
 import de.cketti.mailto.EmailIntentBuilder;
 
 public class MainLiberActivity extends AppCompatActivity implements BookListFragment.OnListFragmentInteractionListener {
@@ -41,13 +41,12 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
     private Toolbar toolbar;
     private AlertDialog.Builder locationAlert;
     private TextView locationTxt;
-    //UserSessionManager session;
-    LinearLayout linearLayout;
-    DatabaseHelper databaseHelper;
-    Intent i;
-    String city;
+    private LinearLayout linearLayout;
+    private String city;
     private SharedPreferences pref;
     private boolean doubleBackToExitPressedOnce = false;
+    private LocationHelper locationHelper;
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,39 +57,13 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
         SharedPreferences.Editor sharedPreferencesEditor =
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
 
+        locationHelper = new LocationHelper(getApplicationContext());
+
         locationTxt = (TextView) findViewById(R.id.toolbar_location_id);
-        //check for user permission to access his location
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(MainLiberActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)){
-                ActivityCompat.requestPermissions(MainLiberActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
-            }else {
-                ActivityCompat.requestPermissions(MainLiberActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
-            }
-        }else{
-            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            LocationHelper locationHelper = new LocationHelper(getApplicationContext());
-            try{
-                city = locationHelper.getLocation(location.getLatitude(),location.getLongitude());
-                locationTxt.setText(city);
-                sharedPreferencesEditor.putString("USER_LOCATION",city);
-                sharedPreferencesEditor.apply();
-                Toast.makeText(getApplicationContext(),"Location Found: "+city,Toast.LENGTH_LONG).show();
-            }catch (Exception e){
-                Toast.makeText(getApplicationContext(),"Location Not Found!",Toast.LENGTH_LONG).show();
-            }
-        }
-
-//        i = getIntent();
-//        city = i.getStringExtra("location");
-
-        String location =pref.getString("USER_LOCATION","LOCATION_NOT_FOUND");
-
-        if(location.isEmpty() || location.equals("LOCATION_NOT_FOUND") || locationTxt.getText().equals("")){
-            Toast.makeText(getApplicationContext(),"Please select Location.",Toast.LENGTH_LONG).show();
-            locationTxt.setText("Select Location");
-        }
-
+     
+       if(!runTimePermission()){
+           enableLocation();
+       }
 
         if (!pref.getBoolean(
                 "COMPLETED_ONBOARDING_PREF_NAME", false)) {
@@ -103,8 +76,6 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
         sharedPreferencesEditor.putBoolean(
                 "COMPLETED_ONBOARDING_PREF_NAME", true);
         sharedPreferencesEditor.apply();
-
-        databaseHelper = new DatabaseHelper(getApplicationContext());
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_id);
         tabLayout = (TabLayout) findViewById(R.id.tab);
@@ -127,10 +98,6 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
 //            alert.show();
 //        }
 
-//        session = new UserSessionManager(getApplicationContext());
-//        session.checkLogin();
-
-        //Add fragment here
         adapter.addFragment(new LibraryFragment(), "Library");
         adapter.addFragment(new BookshelveFragment(), "Bookshelf");
 
@@ -139,7 +106,70 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        //getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    private boolean runTimePermission() {
+
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION }, 100);
+            onResume();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    String loc = intent.getStringExtra("cordinates");
+                    String longitude =loc.substring(0,loc.indexOf(" "));
+                    String latitude = loc.substring(loc.indexOf(" "),loc.length());
+                    if(longitude != null && latitude != null){
+                        city = locationHelper.getLocation(Double.valueOf(latitude), Double.valueOf(longitude));
+                        locationTxt.setText(city);
+                        disableLocation();
+                    }
+                }
+            };
+        }
+
+        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                enableLocation();
+            }else {
+                runTimePermission();
+            }
+        }
+    }
+
+    private void enableLocation() {
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        startService(i);
+    }
+
+
+    private void disableLocation() {
+        Intent i = new Intent(getApplicationContext(), LocationService.class);
+        stopService(i);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(broadcastReceiver != null){
+            unregisterReceiver(broadcastReceiver);
+        }
     }
 
     @Override
@@ -163,8 +193,6 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
                 startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
                 break;
             case R.id.logout_id:
-//                session.logoutUser();
-//                finish();
                 break;
             case R.id.aboutus_id:
                 Toast.makeText(getApplicationContext(), "Functionality coming soon.", Toast.LENGTH_LONG).show();
@@ -199,7 +227,6 @@ public class MainLiberActivity extends AppCompatActivity implements BookListFrag
         }
 
         this.doubleBackToExitPressedOnce = true;
-       // Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
         Snackbar snackbar = Snackbar
                 .make(linearLayout, "Please click BACK again to exit", Snackbar.LENGTH_SHORT);
         snackbar.show();
